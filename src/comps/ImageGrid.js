927e9e29de9a93b9f2791f9ff24fa0a9af5b6e40
subaru-one <subaru@SubarunoMacBook-puro-3.local>
2021-05-03 22:49:48 +0900

    creating modal

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import App from '../App';
+import useFirestore from '../hooks/useFirestore';
 
 const ImageGrid = () =>{
-
+    const {docs} = useFirestore('images');
+console.log(docs)
     return (
         <div className ="img-grid">
-            images
+            {docs && docs.map(doc => (
+                <div className ="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt ="uploaded pic" />
+                </div>
+            ))}
         </div>
     )
 }
