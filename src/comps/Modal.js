c7d5ec147800c82fb24779b1ce19d7e047bb9de0
subaru-one <subaru@SubarunoMacBook-puro-3.local>
2021-05-04 23:58:42 +0900

    all  done

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {motion} from 'framer-motion';
 const Modal = ({selectedImg,setSelectedImg}) => {
 
     const handleClick =(e) =>{
@@ -8,9 +8,15 @@ const Modal = ({selectedImg,setSelectedImg}) => {
     }
     }
     return (
-        <div className ="backdrop" onClick={handleClick}>
-            <img src={selectedImg} alt="enlarged pic" />
-        </div>
+        <motion.div className ="backdrop" onClick={handleClick}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        >
+        <motion.img src={selectedImg} alt ="enlargeing"
+        initial={{y: "-100vh"}}
+        animate={{y:0}}
+        />
+        </motion.div>
     )
 }
 
--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,18 +1,22 @@
 import React,{useEffect} from 'react';
 import useStorage from '../hooks/useStorage';
-
+import {motion} from 'framer-motion';
 const ProgressBar =({file, setFile}) =>{
-    const{progress, url} = useStorage(file);
+    const{ progress,url} = useStorage(file);
    
     useEffect(() => {
         if(url){
             setFile(null);
         }
-    },[url,setFile])
+    },[url,setFile]);
 
     return(
-        <div className="progress-bar" style= {{ width: progress + '%'} }></div>
-    )
+        <motion.div className="progress-bar" 
+        initial={{width: 0}}
+        animate={{width: progress + '%'}}
+        >
+        </motion.div>
+    );
 }
 
-export default ProgressBar
+export default ProgressBar;
